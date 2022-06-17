ba17c5cc309589f47f767846bdafc244691a7bcd
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2022-06-18 01:14:08 +0900

    説明の追加
    メソッドの説明をコメントアウトしました。

--- app/src/main/java/io/github/hrt4423/project4423/MainActivity.java
+++ app/src/main/java/io/github/hrt4423/project4423/MainActivity.java
@@ -7,8 +7,8 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate(Bundle savedInstanceState) {//引数：アプリの状態情報
+        super.onCreate(savedInstanceState);//親クラスのメソッドを呼び出しておく。
+        setContentView(R.layout.activity_main);//画面に表示すべきビューを設定する。
     }
 }
