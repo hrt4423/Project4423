e6feb6202c2e37ed698e6a217cf442d6cab950f8
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2022-06-21 14:57:51 +0900

    画像の表示（テスト）
    SVG形式の画像を表示しました。

--- app/src/main/java/io/github/hrt4423/project4423/MainActivity.java
+++ app/src/main/java/io/github/hrt4423/project4423/MainActivity.java
@@ -3,12 +3,23 @@ package io.github.hrt4423.project4423;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //ImageViewクラスの変数
+    private ImageView sq;
     @Override
     protected void onCreate(Bundle savedInstanceState) {//引数：アプリの状態情報
         super.onCreate(savedInstanceState);//親クラスのメソッドを呼び出しておく。
         setContentView(R.layout.activity_main);//画面に表示すべきビューを設定する。
+
+
+
+//findViewByIdでactivity_mainで設定したidからviewを探す。
+        sq = findViewById(R.id.sq);
+
+//setX setY メソッドで座標を設定
+        sq.setX(100.0f);
+        sq.setY(100.0f);
     }
 }
