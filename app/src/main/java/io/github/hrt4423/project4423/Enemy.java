0f883ff4bd64f30f8baf9dd7ce437f3d0a78eea2
hanabonbon <107159047+hanabonbon@users.noreply.github.com>
2022-07-18 16:21:50 +0900

    Enemyメソッドを追加

--- app/src/main/java/io/github/hrt4423/project4423/Enemy.java
+++ app/src/main/java/io/github/hrt4423/project4423/Enemy.java
@@ -1,11 +1,25 @@
 package io.github.hrt4423.project4423;
 
+import android.widget.ImageView;
+
 public class Enemy {
-    String mumin = "mumin";
-    String hirata = "hirata";
+    public static void Enemy1(float enemyX,int enemySize,int screenWidth,int enemySpeed,ImageView enemy){
+        //Enemy 変化量を決めてる
+        enemyX -= enemySpeed;
+
+        //画面外に出たときの処理
+        if (enemyX < -enemySize) {
+            enemyX = screenWidth  + 20;
+            /*Y座標は、0 から frameHeight – orange.getHeight() の範囲のランダムな値。
+            enemyY = (float)Math.floor(Math.random() * (frameHeight - enemy.getHeight()));
+            */
+        }
+
+        //値の更新
+        enemy.setX(enemyX);
+        //enemy.setY(enemyY);
+    }
 
 }
 
-class enemy2{
-    private int enemy2X;
-}
+
