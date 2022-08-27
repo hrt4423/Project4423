f337182efb2b7dfd6dce2f961952261e00176669
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2022-08-27 10:35:44 +0900

    Revert "Merge branch 'main' into Hirata_2"
    This reverts commit c8bf51cebbf50a28e2b6da434ab07194cdcebca9, reversing
    changes made to ed46035b93fe192589e8a3919036e4d44d9191d2.

--- app/src/main/java/io/github/hrt4423/project4423/Enemy3.java
+++ app/src/main/java/io/github/hrt4423/project4423/Enemy3.java
@@ -1,5 +1,5 @@
 package io.github.hrt4423.project4423;
-//敵側でギザギザ動き
+
 public class Enemy3 extends Enemy{
     @Override
     public void setData(ActivityData enemyData, FrameData frameData){
@@ -11,15 +11,15 @@ public class Enemy3 extends Enemy{
     @Override
     public void move(){
         //動く向き
-        if(motionFlgX){
+        /*if(motionFlgX){
             //右
-            eD.setImgX(eD.getImgX() + speed);
+            cD.setImgX(cD.getImgX() + speed);
         }else{
             //左
-            eD.setImgX(eD.getImgX() - speed);
+            cD.setImgX(cD.getImgX() - speed);
         }
 
-
+         */
 
         if(motionFlgY){
             //上
@@ -29,7 +29,26 @@ public class Enemy3 extends Enemy{
             eD.setImgY(eD.getImgY() - speed);
         }
 
-        
+        //画面外の時の処理
+        /*
+        if (cD.getImgX() < 0){
+            cD.setImgX(1);
+            motionFlgX = true;
+        }
+        //画面外のときの処理
+        if (cD.getImgX() > fD.getScreenWidth() - cD.imgWidth) {
+            cD.setImgX(fD.getScreenWidth() - cD.imgWidth);
+            motionFlgX = false;
+        }
+        */
+        if(eD.getImgY() < 0){
+            eD.setImgY(1);
+            motionFlgY = true;
+        }
 
+        if(eD.getImgY() > fD.getFrameHeight() - eD.imgHeight){
+            eD.setImgY(fD.getFrameHeight() - eD.imgHeight);
+            motionFlgY = false;
+        }
     }
 }
