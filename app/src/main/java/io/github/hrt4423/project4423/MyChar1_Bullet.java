e185f78deb4f2a607e7ca110a4f38909d19b4ef2
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2022-08-27 10:34:51 +0900

    Revert "a"
    This reverts commit 9b4a26d170178b3aeda6bcb3f22d3681e147066d.

--- app/src/main/java/io/github/hrt4423/project4423/MyChar1_Bullet.java
+++ app/src/main/java/io/github/hrt4423/project4423/MyChar1_Bullet.java
@@ -1,6 +1,6 @@
 package io.github.hrt4423.project4423;
 
-public class Enemy2_Bullet extends Bullet{
+public class MyChar1_Bullet extends Bullet{
     @Override
     public void setData(ActivityData bulletData, FrameData frameData, ActivityData charData){
         bD = bulletData;
@@ -11,13 +11,15 @@ public class Enemy2_Bullet extends Bullet{
 
     @Override
     protected void move() {
+        float x = bD.getImgX();
+        float y = bD.getImgY();
         //画面外に出た場合、敵キャラの位置に戻す。
         // 座標を指定するだけでは見た目がズレるので補正している。（中心座標を使えばシンプルに書けそう。）
-        if(bD.getImgY() > fD.getFrameHeight()){
-            bD.setImgX(cD.getImgX() + eBCorrectionX);
-            bD.setImgY(cD.getImgY() + eBCorrectionY);
-        }else{//弾を下に動かす
-            bD.setImgY(bD.getImgY() + speed);
+        if(y < 0){
+            bD.setImgY(cD.getImgY());
+            bD.setImgX(cD.getImgX() + mcBCorrection);
+        }else{
+            bD.setImgY(y - speed);
         }
     }
 }
