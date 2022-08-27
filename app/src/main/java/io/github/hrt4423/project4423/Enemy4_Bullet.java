e08e8231d5fdc941ac2f74cd746a9c144b3ad15c
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2022-08-27 10:17:31 +0900

    微変更

--- app/src/main/java/io/github/hrt4423/project4423/Enemy4_Bullet.java
+++ app/src/main/java/io/github/hrt4423/project4423/Enemy4_Bullet.java
@@ -5,7 +5,7 @@ public class Enemy4_Bullet extends Bullet{
     public void setData(ActivityData bulletData1Data, FrameData frameData, ActivityData activityData){
         bD = bulletData1Data;
         fD = frameData;
-        eD = activityData;
+        cD = activityData;
         speed = Math.round(fD.getScreenWidth()/30f);
     }
 
@@ -14,8 +14,8 @@ public class Enemy4_Bullet extends Bullet{
         //画面外に出た場合、敵キャラの位置に戻す。
         // 座標を指定するだけでは見た目がズレるので補正している。（中心座標を使えばシンプルに書けそう。）
         if(bD.getImgY() > fD.getFrameHeight()){
-            bD.setImgX(eD.getImgX() + eBCorrectionX);
-            bD.setImgY(eD.getImgY() + eBCorrectionY);
+            bD.setImgX(cD.getImgX() + eBCorrectionX);
+            bD.setImgY(cD.getImgY() + eBCorrectionY);
         }else{//弾を下に動かす
             bD.setImgY(bD.getImgY() + speed);
         }
